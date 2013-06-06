50a1824c5fa8451ae207947484c08933aea2bdb6
Seven Watt <info@sevenwatt.com>
2013-06-06 23:20:30 +0200

    Merged LaCrosse extension
    Merged LaCrosse extension from github/Rinie/weatherstationFSK

--- README.md
+++ README.md
@@ -2,3 +2,51 @@ wirelessweather
 ===============
 
 receive signals from wireless weather sensors from Fine Offset, Alecto and look alikes with standard and modified Jeenodes and arduino.
+
+WeatherStationFSKv2 is able to receive signals from Fine Offset, Alecto, LaCrosse IT+ and WS1600
+weather stations using the RFM12B receiver on standard JeeNodes. It can only operate at the frequency
+band of the RFM12B. In Europe that will be most of the time 868MHz, for which the code is applicable.
+But alse US 915MHz and globally 433MHz can be used. This requires an adaptation in the setting of the
+RFM12B frequency.
+
+weatherstationFSK uses an adapted version of the RF12 drivers.
+The adapted version isd part of the rf12mods branch of jcw/jeelib in github:
+https://github.com/jcw/jeelib/tree/rf12mods
+It is adapted such that fixed length packages can be received.
+A external function rf12_setFixedLength(uint8_t packet_len) is provided.
+With packet_len > 0 exactly this maximum number of bytes is received, and
+no checks on  rf12_len (= rf12_buf[2]) will be done.
+With packet_len = 0, the driver fucntions as before and is intended to
+receive standard RF12-driver packages.
+
+To install rf12mods: (not recommended for JeeNode micro (JNu) users)
+1. rename ...\Arduino\Libraries\jeelib to ...\Arduino\Libraries\jeelib-original
+2. download jeelib from the rf12mods branch
+      https://github.com/jcw/jeelib/tree/rf12mods
+3. copy this jeelib version to ...\Arduino\Libraries\jeelib
+
+Untested, but alternatively, only the files RF12.h and RF12.cpp can be replaced in:
+   ...\Arduino\Libraries\jeelib
+
+To install the wheatherstation sketch
+1. create a folder weatherstationFSK in the arduino base folder
+      arduino/weatherstationFSKv2
+2. copy weatherstationFSKv2.ino to this folder
+3. when already active, close the arduino IDE
+      added libraries and projects are only seen at startup
+4. start the arduino IDE, and select the sketch weatherstationFSKv2
+
+Use:
+The amount of information printed can be controlled by setting some #defines 
+in the first view lines of the weatherstationFSKv2.ino:
+    //Define the printed output
+    //#define LOGRAW 1 //comment = disable, uncomment = enable logging received package group_id 212 (0xD4)
+    //#define LOGPKT 1 //comment = disable, uncomment = enable logging unique package passing crc
+    #define LOGDCF 1 //comment = disable, uncomment = enable updating time and logging of DCF77 values
+    #define LOGDAT 1 //comment = disable, uncomment = enable logging of parsed sensor data
+
+
+Rinie added IT+ decoding that uses the same speed as FineOffet
+Added WS1600 decoding that uses half the speed.
+See http://fredboboss.free.fr/tx29/index.php?lang=en
+#define ITPLUS1600 for WS1600 decoding
--- WeatherStationFSKv2/WeatherStationFSKv2.ino
+++ WeatherStationFSKv2/WeatherStationFSKv2.ino
@@ -1,4 +1,3 @@
-
 /// FSK weather station receiver
 /// Receive packets echoes to serial.
 /// Updates DCF77 time.
@@ -6,8 +5,9 @@
 /// National Geographic 265 requires adaptation of frequency to 915MHz band.
 /// input handler and send functionality in code, but not implemented or used.
 /// 2013-03-10<info@sevenwatt.com> http://opensource.org/licenses/mit-license.php
-/// v2: adds sensor network functionality: transmits received data using the
+/// v2: adds sensopr network functionality: transmits received data using the
 /// standard JeeLabs network protocol
+/// 2013-03-29 Rinie added IT+ and TI+WS1600 decoding
 
 //#include <JeeLib.h>
 #include <RF12.h>
@@ -26,11 +26,26 @@
 #define LEN_WS3000 9
 #define LEN_MAX 10
 
+#define CRC_LEN_MIN 10
+#define ITPLUS // Rinie
+#ifdef ITPLUS
+#define MSG_UNKNOWN 0
+#define MSG_ITPLUS 3
+#define LEN_ITPLUS 5
+#define CRC_LEN_MIN 5
+#define MSG_ITPLUS1600 4
+#define ITPLUS1600
+#undef ITPLUS1600 // fine offset and IT+ TX37/29
+#ifdef ITPLUS1600
+#define LEN_MAX 16
+#endif
+#endif
+
 #define SERIAL_BAUD 57600
 //#define LED_PIN     9   // activity LED, comment out to disable
 
 #define NODE_ID 22 //any node works wih modified driver. Use 31 for unmodified driver.
-#define GROUP_ID 178
+#define GROUP_ID 212
 #define DEST_NODE_ID 1 //1=central node, 0=broadcast
 
 static void activityLed (byte on) {
@@ -47,9 +62,10 @@ static unsigned long ok_ts;
 static byte packet_found = 0;
 static byte ok_cnt = 0;
 static byte pkt_cnt = 0;
-static uint8_t packet[10];
+static uint8_t packet[LEN_MAX];
 static uint8_t msgformat = 0;
 static uint8_t pktlen = LEN_MAX;
+
 static uint8_t txcnt = 0;
 
 //void configureWH1080 () {
@@ -81,6 +97,10 @@ void configureWH1080 () {
     rf12_restore(NODE_ID, RF12_868MHZ, 0xD4); // group 212
     rf12_setBitrate(0x13);                    // 17.24 kbps
     rf12_control(0xA67C);                     // 868.300 MHz
+#ifdef ITPLUS1600
+    //rf12_control(0xC623); // 17.24 kbps ws1600 try POR
+    rf12_control(0xC628);   // half speed WS1600 see http://www.g-romahn.de/ws1600
+#endif
     rf12_setFixedLength(LEN_MAX);             // receive fixed number of bytes  
 }
 
@@ -111,12 +131,15 @@ static void do_tests() {
 
 void setup() {
     Serial.begin(SERIAL_BAUD);
-    Serial.print("\n[weatherstationFSK]\n");
-    
+
+#ifndef ITPLUS1600
+    Serial.println("[weatherstationFSK]/n");
+#else
+    Serial.println("[weatherstationWS1600FSK]/n");
+#endif
     activityLed(0);
     rf12_initialize(NODE_ID, RF12_868MHZ, GROUP_ID); //reset RFM12B and initialize
     configureWH1080();
-    
     //test
     //do_tests();
 }
@@ -136,7 +159,27 @@ void loop() {
             pktlen = LEN_WS3000;
             break;
           }
+          case 0x9: { // it+ fast
+            crc_ok = rf12_buf[5] == _crc8(&rf12_buf[1], 4);
+            if (crc_ok) ok_cnt++;
+            msgformat = MSG_ITPLUS;
+            pktlen = LEN_ITPLUS;
+            break;
+          }
           case 0xA:
+#ifdef ITPLUS1600
+          {
+              byte datasets = rf12_buf[2] & 0x0F; // 2bytes, 4 nibbles
+              if (datasets <= 5) { // odd, blank lower 0 of last byte
+                pktlen = datasets * 2 + 2 + 1;
+                crc_ok = rf12_buf[pktlen] == _crc8(&rf12_buf[1], pktlen-1);
+                if (crc_ok) { ok_cnt++;
+                  msgformat = MSG_ITPLUS1600;
+                }
+              }
+            break;
+          }
+#endif
           case 0xB: {
             crc_ok = rf12_buf[10] == _crc8(&rf12_buf[1], 9);
             if (crc_ok) ok_cnt++;
@@ -144,9 +187,11 @@ void loop() {
             pktlen = LEN_WS4000;
             break;
           }
-          default: break; //crc_ok=0;
+          default:
+            msgformat = MSG_UNKNOWN;
+            break; //crc_ok=0;
         }
-        
+
 #ifdef LOGRAW
         //Log all packages. Packages may be missed due to short intervals
         Serial.print(crc_ok ? " ok " : "nok ");
@@ -158,18 +203,21 @@ void loop() {
         Serial.println();
         //Serial.println(millis());
 #endif
-        
+
         //save the first crc_ok package of a burst
         if ((!packet_found) && crc_ok){
           //start one second interval to count upto six identical packets.
           ok_ts=millis();
           packet_found = 1; //true
-          memcpy(packet, (char *)&rf12_buf[1], 10);
+          memcpy(packet, (char *)&rf12_buf[1], LEN_MAX);
         }
     }
-    
+
     //Report if transmission is finished (38ms after first package detected)
-    if (packet_found && ((long)(millis()-ok_ts) > 50)) {
+    if (packet_found &&
+        (((msgformat == MSG_ITPLUS) || (msgformat == MSG_ITPLUS1600)) || 
+        ((long)(millis()-ok_ts) > 50))) //WS3000/WS4000 have multiple transmissions
+    {
       uint8_t mt = packet[0] >> 4;
 #ifdef LOGDCF
       //Set time if time packet received
@@ -186,7 +234,7 @@ void loop() {
       Serial.print(pkt_cnt);
       Serial.print(" ok_cnt: ");
       Serial.print(ok_cnt);
-      Serial.print(" pkt: ");      
+      Serial.print(" pkt: ");
       for (byte i = 0; i < pktlen; i++) {
           //Serial.print(' ');
           Serial.print(packet[i] >> 4, HEX);
@@ -195,7 +243,17 @@ void loop() {
       }
       Serial.println();
 #endif
+
 #ifdef LOGDAT
+#ifdef ITPLUS
+      if (msgformat == MSG_ITPLUS) {
+        decodeSensorDataItPlus(packet);
+      }
+      else if (msgformat == MSG_ITPLUS1600) {
+        decodeSensorDataWs1600(packet);
+      }
+      else if (ok_cnt > 1)
+#endif
       if (mt == 0xA || mt == 0x5) {
         timestamp();
         decodeSensorData(msgformat, packet);
@@ -228,30 +286,29 @@ void loop() {
 */
 uint8_t _crc8(volatile uint8_t *addr, uint8_t len)
 {
-	uint8_t crc = 0;
-
-	// Indicated changes are from reference CRC-8 function in OneWire library
-	while (len--) {
-		uint8_t inbyte = *addr++;
-		uint8_t i;
-		for (i = 8; i; i--) {
-			uint8_t mix = (crc ^ inbyte) & 0x80; // changed from & 0x01
-			crc <<= 1; // changed from right shift
-			if (mix) crc ^= 0x31;// changed from 0x8C;
-			inbyte <<= 1; // changed from right shift
-		}
-	}
-	return crc;
+  uint8_t crc = 0;
+  // Indicated changes are from reference CRC-8 function in OneWire library
+  while (len--) {
+    uint8_t inbyte = *addr++;
+    uint8_t i;
+    for (i = 8; i; i--) {
+      uint8_t mix = (crc ^ inbyte) & 0x80; // changed from & 0x01
+      crc <<= 1; // changed from right shift
+      if (mix) crc ^= 0x31;// changed from 0x8C;
+      inbyte <<= 1; // changed from right shift
+    }
+  }
+  return crc;
 }
 
 void timestamp()
 {
-  Serial.print(year()); 
+  Serial.print(year());
   Serial.print("-");
   printDigits(month());
   Serial.print("-");
   printDigits(day());
-  Serial.print(" "); 
+  Serial.print(" ");
   printDigits(hour());
   Serial.print(":");
   printDigits(minute());
@@ -281,6 +338,7 @@ void update_time(uint8_t* tbuf) {
 char* formatDouble( double val, byte precision, char* ascii, uint8_t ascii_len){
   // formats val with number of decimal places determine by precision
   // precision is a number from 0 to 6 indicating the desired decimial places
+
   
   snprintf(ascii,ascii_len,"%d",int(val));
   if( precision > 0) {
@@ -290,6 +348,7 @@ char* formatDouble( double val, byte precision, char* ascii, uint8_t ascii_len){
     byte padding = precision -1;
     while(precision--)
        mult *=10;
+
        
     if(val >= 0)
       frac = (val - int(val)) * mult;
@@ -316,7 +375,7 @@ void decodeSensorData(uint8_t fmt, uint8_t* sbuf) {
     int16_t temp = ((sbuf[1] & 0x07) << 8) | sbuf[2];
     if (sign)
       temp = (~temp)+sign;
-    double temperature = temp * 0.1; 
+    double temperature = temp * 0.1;
     //humidity
     uint8_t humidity = sbuf[3] & 0x7F;
     //wind speed
@@ -360,4 +419,221 @@ void decodeSensorData(uint8_t fmt, uint8_t* sbuf) {
               rstr);
     }
     Serial.println(str);
+}
+
+
+void PrintChar(byte S) {
+  Serial.write(S);
+ }
+
+void PrintComma(void)
+  {
+  Serial.print(", ");
+  }
+
+void PrintNum(uint16_t x, char c, byte digits) {
+     // Rinie add space for small digits
+     if(c) {
+      PrintChar(c);
+  }
+  for (uint16_t i=1, val=10; i < digits; i++, val *= 10) {
+    if (x < val) {
+      PrintChar(' ');
+    }
+  }
+
+    Serial.print(x,DEC);
+}
+
+void PrintNumHex(uint16_t x, char c, byte digits) {
+  // Rinie add space for small digits
+  if(c) {
+    PrintChar(c);
+  }
+  for (uint16_t i=1, val=16; i < digits; i++, val *= 16) {
+    if (x < val) {
+      PrintChar('0');
+    }
+  }
+
+  Serial.print(x,HEX);
+}
+
+void PrintByteHex(uint8_t x, char c) {
+  // Rinie add space for small digits
+  PrintNumHex(x, c, 2);
+}
+
+/*
+ * IT Plus
+ *
+ * http://fredboboss.free.fr/tx29/index.php?lang=en
+ *  http://forum.jeelabs.net/node/110
+ * http://gcrnet.net/node/32
+ */
+void decodeSensorDataItPlus(uint8_t* sbuf) {
+    char *compass[] = {"N  ", "NNE", "NE ", "ENE", "E  ", "ESE", "SE ", "SSE", "S  ", "SSW", "SW ", "WSW", "W  ", "WNW", "NW ", "NNW"};
+    uint8_t windbearing = 0;
+    // station id
+    uint8_t stationid = ((sbuf[0] & 0x0F) << 4) | ((sbuf[1] & 0xC0) >>4);
+    uint8_t newBattery = (sbuf[1] & 0x20) >> 5;
+
+    int8_t temp;
+    int8_t tempDeci;
+    //humidity
+    uint8_t humidity = (sbuf[3]) & 0x7F;
+    uint8_t lowBat = (sbuf[3]) & 0x80 >> 7;
+
+  temp = BCD2bin(sbuf[1] & 0x0F) * 10 + BCD2bin((sbuf[2] & 0xF0)>>4);
+  temp = temp - 40;
+  tempDeci = BCD2bin((sbuf[2] & 0x0F));
+
+    Serial.print("IT+ ID: ");
+    Serial.print(stationid, HEX);
+  Serial.print(" Temp: ");
+  Serial.print(temp);
+  PrintChar('.');
+  PrintNum(tempDeci, 0, 1);
+  if (humidity < 106) {
+    Serial.print(" Humidity:");
+    PrintNum(humidity, ' ', 2);
+  }
+
+  if (newBattery) {
+    Serial.print(" N");
+  }
+  else if (lowBat) {
+    Serial.print(" L");
+  }
+
+  Serial.print(" RawData:");
+  for (byte i = 0; i < MSG_ITPLUS; i++) {
+    PrintByteHex(sbuf[i], ' ');
+  }
+    Serial.println();
+}
+
+
+/*
+http://www.g-romahn.de/ws1600/Datepakete_raw.txt
+
+Data - organized in nibbles - are structured as follows (exammple with blanks added for clarity):
+
+ a 5a 5 0 628 1 033 2 000 3 e00 4 000 bd
+
+ data always start with "a"
+ from next 1.5 nibbles (here 5a) the 6 msb are identifier of transmitter,
+ bit 1 indicates acquisition/synchronizing phase (so 5a >> 58 thereafter)
+ bit 0 will be 1 in case of error (e.g. no wind sensor 5a >> 5b)
+ next nibble (here 5) is count of quartets to betransmitted
+ up to 5 quartets of data follow
+ each quartet starts with type indicator (here 0,1,2,3,4)
+ 0: temperature, 3 nibbles bcd coded tenth of °c plus 400 (here 628-400 = 22.8°C)
+ 1: humidity, 3 nibbles bcd coded (here 33 %rH), meaning of 1st nibble still unclear
+ 2: rain, 3 nibbles, counter of contact closures
+ 3: wind, first nibble direction of wind vane (multiply by 22.5 to obtain degrees,
+    here 0xe*22.5 = 315 degrees)
+  next two nibbles wind speed in m per sec (i.e. no more than 255 m/s; 9th bit still not found)
+ 4: gust, speed in m per sec (yes, TX23 sensor does measure gusts and data are transmitted
+    but not displayed by WS1600), number of significant nibbles still unclear
+ next two bytes (here bd) are crc.
+ During acquisition/synchronizing phase (abt. 5 hours) all 5 quartets are sent, see examplke above. Thereafter
+ data strings contain only a few ( 1 up ton 3) quartets, so data strings are not! always of
+ equal length.
+
+After powering on, the complete set of data will be transmitted every 4.5 secs for 5 hours during acquisition phase.
+Lateron only selected sets of data will be transmitted.
+
+Stream of received data follows:
+
+1st line: raw data in hex format as received from sensors
+2nd line: meteorological data from outdoor sensors decoded, Same values
+    are displayed on basestation (last duet is "calculated crc" - always 00).
+
+a1250444109120003808401b89 00
+Temp  044 Humi 91 Rain 000 Wind 028  Dir 180 Gust 097  ( 4.4 °C, 91 %rH, no rain, wind 2.8 km/h from south, gust 9.7 km/h)
+*/
+
+void decodeSensorDataWs1600(uint8_t* sbuf) {
+    char *compass[] = {"N  ", "NNE", "NE ", "ENE", "E  ", "ESE", "SE ", "SSE", "S  ", "SSW", "SW ", "WSW", "W  ", "WNW", "NW ", "NNW"};
+    uint8_t windbearing = 0;
+    // station id
+    uint8_t stationid = ((sbuf[0] & 0x0F) << 6) | ((sbuf[1] & 0xC0) >>6);
+    uint8_t dataSets = sbuf[1] & 0xF;
+    int8_t temp;
+    int8_t tempDeci;
+    //humidity
+    uint8_t humidity = 0;
+    //wind speed
+    uint8_t windspeed = 0;
+    //wind gust
+    uint8_t windgust = 0;
+    //rainfall
+    uint16_t rain = 0;
+
+    Serial.print("IT+WS1600 ID: ");
+    Serial.print(stationid, HEX);
+
+    for (byte i = 0; i < dataSets; i++) {
+    byte j = 2 + i*2;
+    byte sensorType = (sbuf[j] & 0xF0) >> 4;
+    switch (sensorType) { // e.g. a 5a 5 0 628 1 033 2 000 3 e00 4 000 bd
+      case 0: //  0: temperature, 3 nibbles bcd coded tenth of °c plus 400 (here 628-400 = 22.8°C)
+        temp = BCD2bin(sbuf[j] & 0x0F) * 10 + BCD2bin((sbuf[j + 1] & 0xF0)>>4);
+        temp = temp - 40;
+        tempDeci = BCD2bin((sbuf[j + 1] & 0x0F));
+        Serial.print(" Temp: ");
+        Serial.print(temp);
+        PrintChar('.');
+        PrintNum(tempDeci, 0, 1);
+        break;
+      case 1: // 1: humidity, 3 nibbles bcd coded (here 33 %rH), meaning of 1st nibble still unclear
+        humidity = BCD2bin(sbuf[j + 1]);
+        Serial.print(" Humidity:");
+        PrintNum(humidity, ' ', 2);
+        break;
+      case 2: // 2: rain, 3 nibbles, counter of contact closures
+        rain = ((sbuf[j] & 0x0F) + (sbuf[j + 1]) * 100);
+        Serial.print(" Rain:");
+        PrintNum(rain, ' ', 3);
+        break;
+      case 3: //3: wind, first nibble direction of wind vane (multiply by 22.5 to obtain degrees,
+            // here 0xe*22.5 = 315 degrees)
+          // next two nibbles wind speed in m per sec (i.e. no more than 255 m/s; 9th bit still not found)
+        windbearing = (sbuf[j] & 0x0F);
+        windspeed = (sbuf[j + 1]);
+        Serial.print(" Windbearing:");
+        PrintNum(windbearing, ' ', 2);
+        Serial.print(" Windspeed:");
+        if (windspeed >= 0xFE) {
+          Serial.print("---");
+        }
+        else {
+          PrintNum(windspeed, ' ', 3);
+        }
+        break;
+      case 4: // 4: gust, speed in m per sec (yes, TX23 sensor does measure gusts and data are transmitted
+            // but not displayed by WS1600), number of significant nibbles still unclear
+        windgust = ((sbuf[j] & 0x0F) * 256 + (sbuf[j + 1]));
+        Serial.print(" Gust:");
+        if (windgust >= 0xFE) {
+          Serial.print("---");
+        }
+        else {
+          PrintNum(windgust, ' ', 3);
+        }
+        break;
+      default:
+        Serial.print(" Unknown: ");
+        PrintNumHex(sensorType, 0, 1);
+        PrintNumHex(((sbuf[j] & 0x0F) * 256 + (sbuf[j + 1])), ' ', 3);
+        break;
+    }
+
+  }
+  Serial.print(" RawData:");
+  for (byte i = 0; i < dataSets * 2 + 3; i++) {
+    PrintByteHex(sbuf[i], ' ');
+  }
+    Serial.println();
 }
